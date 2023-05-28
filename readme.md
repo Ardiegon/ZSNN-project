c7975bbed1681451249964469cba311a010867ac
Ardiegon <oskbsz@gmail.com>
2023-05-28 18:33:26 +0200

    simple dataloader

---
+++ readme.md
@@ -0,0 +1,9 @@
+# ZSNN Projekt
+Image generation with diffusion models
+
+## Instalation
+```bash
+conda env create -f env.yml
+conda activate zsnn-env
+conda develop src 
+```
---
+++ src/configs/path.py
@@ -0,0 +1,4 @@
+import os 
+
+DATASET_DIR_PATH = os.path.join("Track2","data", "Console_sliced", "train")
+ANNOTATIONS_PATH = os.path.join(DATASET_DIR_PATH, "_annotations.coco.json") 
